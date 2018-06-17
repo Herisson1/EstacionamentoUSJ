8f8925c6a8d97b67563d330f5f6df8c4a4708710
Herisson Paulo Ferreira <heerisson@gmail.com>
2018-06-17 14:29:19 -0300

    Validações & Criação do Servlet
    Limitação no campo Placa, Criação do Servlet de Veículos, Criação de um modelo de banco alternativo, Criação do arquivo Scrum

---
+++ SQL/EstacionamentoDB.sql
@@ -0,0 +1,14 @@
+--DROP DATABASE IF EXISTS EstacionamentoDB;
+
+CREATE DATABASE EstacionamentoDB;
+
+USE EstacionamentoDB;
+
+
+CREATE TABLE Veiculos (
+	id INT NOT NULL AUTO_INCREMENT,
+	Motorista VARCHAR(55) NOT NULL,
+	Placa VARCHAR(10) NOT NULL,
+	TipoVeiculos VARCHAR(20),
+	PRIMARY KEY (ID)
+);
